55397e772123e9f22e14d263592e573a3692d69d
Michele Marschner <michele.marschner@gmail.com>
2020-08-06 07:35:42 +0200

    changed Port for herku deployment

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const connectDB = require('./config/db');
 dotenv.config( { path: './config/config.env' } );
 connectDB();
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.use('/api/todos', toDoRouter);
-
+app.get('/favicon.ico', (req, res) => res.status(204));
 /*app.get((req, res,) => {
   let err = new Error(`${req.ip} tried to reach ${req.originalUrl}`); // Tells us which IP tried to reach a particular URL
   err.statusCode = 404;
